6274153a25466f55eaeba9cf9daf0895d55435bf
T1T4N119 <pududasuka625@gmail.com>
2025-07-29 09:17:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/CpfTPVz7",
+ALIVE_MSG: process.env.ALIVE_MSG || "┏━━━━━━━━━━━➤◍◉➤\n│ *🔰𝐇𝐞𝐲 𝐈'𝐦 𝐂𝐇𝐀𝐌𝐈-𝐌𝐃 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n┗━━━━━━━━━━━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰❤️*\n┗━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐒𝐇𝐀𝐍𝐔𝐖𝐀 𝐌𝐃|*\n┗━━━━━━━━━━━━━━━━━━━━\n*2024🤍*\n\n┏━━━━━━━━━━━➤◍◉➤\n*This bot is made by Shanuka Shameen.This is made with using Ai.You can use this bot easily.New updates will coming*\n┗━━━━━━━━━━━━━━━━━━━━",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+BOT_NUMBER: process.env.BOT_NUMBER || "94766315540",
+OWNER_REACT: process.env.OWNER_REACT || "👨‍💻",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
+READ_CMD: process.env.READ_CMD || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
+BOT_NAME: process.env.BOT_NAME || "➺CHAMI-MD࿐",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
+ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
+ANTI_LINK:process.env.ANTI_LINK || "true",
+ANTI_BAD:process.env.ANTI_BAD || "true",
+ANTI_BOT:process.env.ANTI_BOT || " true"
+};
